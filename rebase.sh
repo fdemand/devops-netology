1990a43905892197ddec568db164d455ee3f88a7
fdemand <fdemand@mail.ru>
2021-11-04 11:58:10 +0300

    rebase modified in main

--- rebase.sh
+++ rebase.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 #script shows command line options
 
+#count=1
+#for param in "$*"; do
+#	echo "\$* Parameter #$count = $param"
+#	count=$(( $count + 1 ))
+#done
+
 count=1
-for param in "$*"; do
-	echo "\$* Parameter #$count = $param"
-	count=$(( $count + 1 ))
+for param in "$@"; do
+	echo "\$@ Parameter #$count = $param"
+	count=$(( $count + 1))
 done
+
+echo "====="
