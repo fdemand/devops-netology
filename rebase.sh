8c29ba986f5fb1c982c3705aabf872eb4ff16ef3
fdemand <fdemand@mail.ru>
2021-11-04 13:13:31 +0300

    git rebase 2 next commit -- operation rebase

--- rebase.sh
+++ rebase.sh
@@ -9,11 +9,13 @@
 
 count=1
 for param in "$@"; do
-	echo "\$@ Parameter #$count = $param"
-	count=$(( $count + 1))
+	echo "Next parameter: $param"
+	count=$(( $count + 1 ))
 done
 
-	count=$(( $count + 1 ))
 
 
 echo "====="
+
+
+echo "git rebase 3"
